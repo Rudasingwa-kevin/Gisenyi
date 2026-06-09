6ed2e17296129172423e6349af5f44d5b38babc8
Rudasingwa-kevin <kevincracker02@gmail.com>
2026-06-09 21:20:59 +0200

    feat: implement interactive map page using Leaflet and integrate into App routing

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import MapView from '../components/MapView';
 
-const MapPage = ({ places, activeCat, selectedPlace, setSelectedPlace }) => {
+const MapPage = ({ places, activeCat, setActiveCat, selectedPlace, setSelectedPlace }) => {
     return (
         <div className="pt-32">
             <MapView 
                 places={places} 
                 activeCat={activeCat} 
+                setActiveCat={setActiveCat}
                 selectedPlace={selectedPlace}
                 setSelectedPlace={setSelectedPlace}
             />
