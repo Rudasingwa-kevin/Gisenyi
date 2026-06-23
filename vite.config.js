01c8688e5ae49f07b080655b69dd9ca7601f661c
Rudasingwa-kevin <kevincracker02@gmail.com>
2026-06-23 15:02:18 +0200

    chore: remove vite-plugin-prerender dependency and configuration

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import prerender from 'vite-plugin-prerender'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    prerender({
-      routes: ['/', '/stays', '/history', '/map', '/gallery', '/events', '/calendar'],
-      basename: '/',
-    }),
   ],
   server: {
     proxy: {
